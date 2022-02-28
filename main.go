2e435ba092291ab5c4ee9664fa061ca5903d58d1
estefania.mantilla.ec@gmail.com <estefania.mantilla.ec@gmail.com>
2022-02-28 16:01:15 -0500

    registro de usuario

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-func main(){
+import (
+	"log"
 
-	
+	"github.com/estefania552/backend-app-itq/bd"
+	"github.com/estefania552/backend-app-itq/handlers"
+)
+
+func main() {
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexión a la BD")
+		return
+	}
+	handlers.Manejadores()
 }
