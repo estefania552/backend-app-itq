182a2d84d36684ca4410e4ecad022f053dc2dc16
estefania.mantilla.ec@gmail.com <estefania.mantilla.ec@gmail.com>
2022-03-07 02:45:33 -0500

    segundo commit

---
+++ bd/insertoRelacion.go
@@ -0,0 +1,24 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/estefania552/backend-app-itq/models"
+)
+
+/*InsertoRelacion graba la relación en la BD */
+func InsertoRelacion(t models.Relacion) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("backend-database-itq")
+	col := db.Collection("relacion")
+
+	_, err := col.InsertOne(ctx, t)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
